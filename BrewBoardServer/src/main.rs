154a60ab61b75c71d8d4ed6491c5b288fe2e7c2a
Theodor Teslia <theoteslia@gmail.com>
2026-03-22 18:42:06 +0100

    Add database connection and insertion

--- BrewBoardServer/src/main.rs
+++ BrewBoardServer/src/main.rs
@@ -1,13 +1,23 @@
-use axum::extract::{Query};
+use axum::extract::{Query, Multipart, State};
 use axum_extra::extract::{Form};
 use axum::response::{Html, Redirect};
 use axum::routing::{get};
 use axum::Router;
 use serde::Deserialize;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+use sqlx::{Pool, Postgres};
+use sqlx::postgres::PgPoolOptions;
+use std::env;
+
+#[derive(Clone, Debug)]
+struct AppState {
+    pool: Pool<Postgres>
+}
 
 #[tokio::main]
 async fn main() {
+    dotenvy::dotenv().ok();
+
     // Thingy for debug printing I think?
     tracing_subscriber::registry()
         .with(
@@ -17,11 +27,19 @@ async fn main() {
         .with(tracing_subscriber::fmt::layer())
         .init();
 
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect("postgresql://localhost:5432/brewboarddb").await.unwrap();
+
+    let state = AppState { pool };
+
     // build our application with some routes
     let app = Router::new()
         .route("/", get(get_root))
         .route("/pour_question", get(get_pour_question))
-        .route("/manual_recipe", get(get_manual_recipe).post(accept_new_manual_recipe));
+        .route("/manual_recipe", get(get_manual_recipe).post(accept_new_manual_recipe))
+        .route("/json_recipe", get(get_json_recipe).post(accept_json_recipe))
+        .with_state(state);
 
     // run it
     let listener = tokio::net::TcpListener::bind("127.0.0.1:1234")
@@ -67,7 +85,6 @@ async fn get_pour_question() -> Html<&'static str> {
                     </label>
                     <br>
                     <input type="text" name="pour_number">
-                    <br>
                     <input type="submit" value="Continue">
                 </form>
             </body>
@@ -86,6 +103,7 @@ async fn get_manual_recipe(Query(q): Query<PourQuestionInput>) -> Html<String> {
     <!DOCTYPE html>
     <html>
         <form method="POST">
+            Name: <input type="text" name="name">
             <table>
                 <tr>
                     <th>
@@ -118,7 +136,7 @@ async fn get_manual_recipe(Query(q): Query<PourQuestionInput>) -> Html<String> {
     }
     new_page.push_str(r#"
             </table>
-        <input type="submit" value="Speichern">
+        <input type="submit" value="Save">
     </form>
 </html>"#);
 
@@ -126,18 +144,97 @@ async fn get_manual_recipe(Query(q): Query<PourQuestionInput>) -> Html<String> {
 }
 
 
+async fn get_json_recipe() -> Html<&'static str> {
+    Html(
+        r#"
+        <!DOCTYPE html>
+        <html>
+            <form method="POST" enctype="multipart/form-data">
+                <input type="file" name="file" accept="application/json">
+                <input type="submit" value="Save">
+            </form>
+        </html>
+        "#
+    )
+}
+
 
 #[derive(Debug, Deserialize, Clone)]
 #[allow(dead_code)]
 struct ManualRecipe {
-    min: Vec<u32>,
-    sec: Vec<u32>,
-    target: Vec<u32>,
+    name: String,
+    min: Vec<i32>,
+    sec: Vec<i32>,
+    target: Vec<i32>,
     note: Vec<String>
 }
 
-async fn accept_new_manual_recipe(Form(manual_recipe): Form<ManualRecipe>) -> Redirect {
-    // Change by adding to database instead
-    dbg!(manual_recipe);
+impl ManualRecipe {
+    fn from_recipe(recipe: Recipe) -> ManualRecipe {
+        let mut result = ManualRecipe {
+            name: recipe.name,
+            min: vec![], sec: vec![], target: vec![], note: vec![]
+        };
+
+        for pour in recipe.pours {
+            result.min.push(pour.min);
+            result.sec.push(pour.sec);
+            result.target.push(pour.target);
+            result.note.push(pour.note);
+        }
+
+        result
+    }
+
+    async fn insert_into_database(&self, state: &AppState) {
+        let _ = sqlx::query!(
+            "
+            INSERT INTO recipe (name, minutes, seconds, targets, notes) VALUES (
+                $1, $2, $3, $4, $5
+            )",
+            &self.name,
+            &self.min,
+            &self.sec,
+            &self.target,
+            &self.note
+        )
+        .execute(&state.pool).await;
+    }
+}
+
+async fn accept_new_manual_recipe(State(state): State<AppState>, Form(manual_recipe): Form<ManualRecipe>) -> Redirect {
+    manual_recipe.insert_into_database(&state).await;
+
+    Redirect::to("/")
+}
+
+#[derive(Debug, Deserialize, Clone)]
+struct Pour {
+    min: i32,
+    sec: i32,
+    target: i32,
+    note: String
+}
+
+#[derive(Debug, Deserialize, Clone)]
+struct Recipe {
+    name: String,
+    pours: Vec<Pour>
+}
+
+
+
+
+async fn accept_json_recipe(State(state): State<AppState>, mut multipart: Multipart) -> Redirect {
+    while let Some(field) = multipart.next_field().await.unwrap() {
+        if field.name() == Some("file") {
+            let data = field.bytes().await.unwrap();
+
+            let parsed: Recipe = serde_json::from_slice(&data).unwrap();
+
+            ManualRecipe::from_recipe(parsed).insert_into_database(&state).await;
+        }
+    }
+
     Redirect::to("/")
 }
